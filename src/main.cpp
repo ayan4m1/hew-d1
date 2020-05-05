57a6277bfe301f246af8771b410c9cd25616e431
ayan4m1 <andrew@bulletlogic.com>
2020-05-05 01:22:34 -0400

    rewrite code to reduce block nesting

--- src/main.cpp
+++ src/main.cpp
@@ -160,89 +160,98 @@ void loop() {
   String request;
   StaticJsonDocument<PACKET_SIZE> doc;
 
-  if (client.available()) {
-    request = client.readStringUntil('\n');
-
-    if (request.startsWith("OPTIONS")) {
-      client.println(RESPONSE_OK);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
-      client.println(HEADER_CONNECTION);
-      client.println();
-      client.println();
-      client.stop();
-      return;
-    } else if (!request.startsWith("POST")) {
-      // ensure this is a POST request
-      client.println(RESPONSE_BAD_REQUEST);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
-      client.println(HEADER_CONNECTION);
-      client.println();
-      client.println();
-      client.stop();
-      return;
-    }
-
-    // skip all the headers
-    // note: this is vulerable to DOS attacks
-    while ((currentTime - lastTime) <= HTTP_TIMEOUT_MS) {
-      currentTime = millis();
-      request = client.readStringUntil('\n');
-
-      if (request.startsWith("{")) {
-        break;
-      }
-    }
-
-    // try to deserialize the JSON
-    DeserializationError error = deserializeJson(doc, request);
-
-    if (error) {
-      client.println(RESPONSE_INTERNAL_SERVER_ERROR);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
-      client.println(HEADER_CONNECTION);
-      client.println();
-      client.println(error.c_str());
-      client.println();
-    } else {
-      const uint32_t newColor = pixels->Color(doc["r"], doc["g"], doc["b"]);
-      const uint8_t newBrightness = doc["br"];
-      boolean settingsChanged = false;
-
-      // sync settings if they have changed
-      if (settings.color != newColor) {
-        settings.color = newColor;
-        settingsChanged = true;
-      }
-
-      if (settings.brightness != newBrightness) {
-        settings.brightness = newBrightness;
-        settingsChanged = true;
-      }
-
-      // update pixels and write new settings to EEPROM if necessary
-      if (settingsChanged) {
-        setPixels();
-
-        EEPROM.begin(sizeof(Settings));
-        EEPROM.put(0, settings);
-        EEPROM.commit();
-        EEPROM.end();
-      }
-
-      client.println(RESPONSE_OK);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
-      client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
-      client.println(HEADER_CONTENT_TYPE);
-      client.println(HEADER_CONNECTION);
-      client.println();
-      client.println();
-    }
+  while (!client.available() && (currentTime - lastTime) <= HTTP_TIMEOUT_MS) {
+    currentTime = millis();
+    delay(20);
   }
+
+  request = client.readStringUntil('\n');
+
+  if (request.startsWith("OPTIONS")) {
+    client.println(RESPONSE_OK);
+    client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
+    client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
+    client.println(HEADER_CONNECTION);
+    client.println();
+    client.println();
+    client.stop();
+    return;
+  } else if (!request.startsWith("POST")) {
+    // ensure this is a POST request
+    client.println(RESPONSE_BAD_REQUEST);
+    client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
+    client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
+    client.println(HEADER_CONNECTION);
+    client.println();
+    client.println();
+
+    if (client.connected()) {
+      client.stop();
+    }
+
+    return;
+  }
+
+  // skip all the headers
+  // note: this is vulerable to DOS attacks
+  while (!request.startsWith("{") && (currentTime - lastTime) <= HTTP_TIMEOUT_MS) {
+    currentTime = millis();
+    request = client.readStringUntil('\n');
+  }
+
+  // try to deserialize the JSON
+  DeserializationError error = deserializeJson(doc, request);
+
+  if (error) {
+    client.println(RESPONSE_INTERNAL_SERVER_ERROR);
+    client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
+    client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
+    client.println(HEADER_CONNECTION);
+    client.println();
+    client.println(error.c_str());
+    client.println();
+
+    if (client.connected()) {
+      client.stop();
+    }
+
+    return;
+  }
+
+  client.println(RESPONSE_OK);
+  client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
+  client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
+  client.println(HEADER_CONTENT_TYPE);
+  client.println(HEADER_CONNECTION);
+  client.println();
+  client.println();
 
   if (client.connected()) {
     client.stop();
+  }
+
+  const uint32_t newColor = pixels->Color(doc["r"], doc["g"], doc["b"]);
+  const uint8_t newBrightness = doc["br"];
+  boolean settingsChanged = false;
+
+  // sync settings if they have changed
+  if (settings.color != newColor) {
+    settings.color = newColor;
+    settingsChanged = true;
+  }
+
+  if (settings.brightness != newBrightness) {
+    settings.brightness = newBrightness;
+    settingsChanged = true;
+  }
+
+  // update pixels and write new settings to EEPROM if necessary
+  if (settingsChanged) {
+    setPixels();
+
+    EEPROM.begin(sizeof(Settings));
+    EEPROM.put(0, settings);
+    EEPROM.commit();
+    EEPROM.end();
   }
 }
