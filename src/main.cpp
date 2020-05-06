778fe5fa0a77f60a2daf0486d16810005016c574
ayan4m1 <andrew@bulletlogic.com>
2020-05-05 23:21:29 -0400

    add documentation

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,45 @@
 #include <ESP8266mDNS.h>
 #include <ESP_EEPROM.h>
 
-#define WIFI_AP "bar"
-#define WIFI_PSK "wi9NNYara"
+// Change this to your Wireless SSID
+#define WIFI_AP "SSID"
+// Change this to your WPA2 PSK
+#define WIFI_PSK "ChangeMe"
+
+// Ensure that each device on your network has a unique identifier
 #define DEVICE_IDENTIFIER "puck-a"
 
-#define PACKET_SIZE JSON_OBJECT_SIZE(4) + 10
-
+// Pin D4 on the D1 mini is the same as LED_BUILTIN, but they also
+// chose to use it as the default pin for this shield. If you want
+// to print a translucent shell, you'll want to remap the control pin
+// by cutting the D4 jumper and bridging the gap between another pad
+// and the control line using solder. This keeps the built-in blue LED
+// off.
 #define LED_PIN D4
+
+// If you're using the RGB LED shield, leave this set to seven.
 #define LED_COUNT 7
 #define LED_BRIGHTNESS 0x08
 
+// If your WiFi network is congested, you may need to increase this.
 #define HTTP_TIMEOUT_MS 2e3
+
+// You can customize the HTTP port by changing this value.
 #define HTTP_PORT 80
 #define HTTP_SERVICE "http"
 #define HTTP_PROTOCOL "tcp"
 
+// Shouldn't need to edit any of these
 #define RESPONSE_OK F("HTTP/1.1 200 OK")
 #define RESPONSE_BAD_REQUEST F("HTTP/1.1 400 Bad Request")
 #define RESPONSE_INTERNAL_SERVER_ERROR F("HTTP/1.1 500 Internal Server Error")
 #define HEADER_ACCESS_CONTROL_ALLOW_ORIGIN F("Access-Control-Allow-Origin: *")
 #define HEADER_ACCESS_CONTROL_ALLOW_HEADERS F("Access-Control-Allow-Headers: content-type")
 #define HEADER_CONNECTION F("Connection: close")
-#define HEADER_CONTENT_TYPE F("Content-type: application/json")
+#define HEADER_CONTENT_TYPE_JSON F("Content-type: application/json")
+
+// Calculated using https://arduinojson.org/v6/assistant/
+#define PACKET_SIZE JSON_OBJECT_SIZE(4) + 10
 
 struct Settings {
   uint32_t color;
@@ -51,6 +68,9 @@ Settings settings = Settings();
 
 const uint32_t black = pixels->Color(0, 0, 0);
 
+/**
+ * If WiFi is not connected, allow up to 10 seconds for connection.
+ */
 bool checkWiFi() {
   if (WiFi.status() == WL_CONNECTED) {
     return true;
@@ -75,6 +95,10 @@ bool checkWiFi() {
   return WiFi.status() == WL_CONNECTED;
 }
 
+/**
+ * Periodically attempt to reconnect to WiFi, blinking the LED
+ * in between attempts.
+ */
 void blinkDeath() {
   bool blink = LOW;
   pinMode(LED_BUILTIN, OUTPUT);
@@ -90,9 +114,13 @@ void blinkDeath() {
   }
 }
 
+/**
+ * Set all pixels to the saved brightness/color values.
+ */
 void setPixels() {
   pixels->setBrightness(settings.brightness);
 
+  // note: this code does not support > 256 LEDs
   for (uint8_t i = 0; i < LED_COUNT; i++) {
     pixels->setPixelColor(i, settings.color);
   }
@@ -100,6 +128,9 @@ void setPixels() {
   pixels->show();
 }
 
+/**
+ * Set up hardware, restore settings from EEPROM, start web server.
+ */
 void setup() {
   // turn off built-in LED
   pinMode(LED_BUILTIN, HIGH);
@@ -141,6 +172,14 @@ void setup() {
   EEPROM.end();
 }
 
+/**
+ * 1) Check for WiFi
+ * 2) Check for HTTP connection
+ * 3) Read/parse/respond to HTTP request
+ * 4) If settings changed, update EEPROM
+ *
+ * Need to add authentication.
+ */
 void loop() {
   if (!checkWiFi()) {
     return blinkDeath();
@@ -221,7 +260,7 @@ void loop() {
   client.println(RESPONSE_OK);
   client.println(HEADER_ACCESS_CONTROL_ALLOW_ORIGIN);
   client.println(HEADER_ACCESS_CONTROL_ALLOW_HEADERS);
-  client.println(HEADER_CONTENT_TYPE);
+  client.println(HEADER_CONTENT_TYPE_JSON);
   client.println(HEADER_CONNECTION);
   client.println();
   client.println();
